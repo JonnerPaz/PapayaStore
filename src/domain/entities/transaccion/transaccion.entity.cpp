3d4a4e39f208f0ebd2c9f1e5f8d2e2c9358c6fe2
JonnerPaz <jonnerpazp@gmail.com>
2026-04-03 14:24:05 -0400

    feat(Transaccion): Add DTO for data reliability
    * Se agregó `TransaccionDTO` para manejar lo que antes se manejaba en
      varias propiedades sueltas, haciendo más dificil sincronizar las
    invariantes y más propenso a errores.
    * Algunos setters fueron eliminados y se agregaron otros para usar los
      transaccionDTO en lugar de los setters a propiedades sueltas
    * Ahora casi todas las propiedades usan la member initialization lists
      en lugar de inicializarse en el ctor (a excepción de descripción, que
    al ser un array fijo no puede inicializarse de esa forma)

--- src/domain/entities/transaccion/transaccion.entity.cpp
+++ src/domain/entities/transaccion/transaccion.entity.cpp
@@ -3,77 +3,65 @@
 Transaccion::Transaccion(int id, char* nombre, bool eliminado,
                          time_point<system_clock> fechaCreacion,
                          time_point<system_clock> fechaUltimaModificacion, TipoDeTransaccion tipo,
-                         int idRelacionado, float total, char* descripcion, int productosIds[],
-                         int cantidades[], float preciosUnitarios[], int cantidadTiposDeProductos)
-    : EntidadBase(id, nombre, eliminado, fechaCreacion, fechaUltimaModificacion)
+                         int idRelacionado, float total, char* descripcion,
+                         TransaccionDTO& productos)
+    : EntidadBase(id, nombre, eliminado, fechaCreacion, fechaUltimaModificacion),
+      m_tipo(tipo),
+      m_idRelacionado(idRelacionado),
+      m_total(total),
+      m_productos(productos)
 {
-    this->tipo = tipo;
-    this->idRelacionado = idRelacionado;
-    this->total = total;
-    this->cantidadTiposDeProductos = cantidadTiposDeProductos;
-    this->descripcion[0] = '\0';
-    this->copiarCadenaSeguro(this->descripcion, 200, descripcion);
-    for (int i = 0; i < 100; ++i) {
-        this->productosIds[i] = productosIds[i];
-        this->cantidades[i] = cantidades[i];
-        this->preciosUnitarios[i] = preciosUnitarios[i];
-    }
+    EntidadBase::copiarCadenaSeguro(this->m_descripcion, sizeof(this->m_descripcion), descripcion);
 }
 
 bool Transaccion::setTipoTransaccion(TipoDeTransaccion nuevoTipo)
 {
-    this->tipo = nuevoTipo;
+    this->m_tipo = nuevoTipo;
     return true;
 }
 
 bool Transaccion::setIdRelacionado(int nuevoIdRelacionado)
 {
-    this->idRelacionado = nuevoIdRelacionado;
+    this->m_idRelacionado = nuevoIdRelacionado;
     return true;
 }
 
 bool Transaccion::setTotal(float nuevoTotal)
 {
-    this->total = nuevoTotal;
+    this->m_total = nuevoTotal;
     return true;
 }
 
 bool Transaccion::setDescripcion(char* nuevaDescripcion)
 {
-    if (!EntidadBase::copiarCadenaSeguro(this->descripcion, 200, nuevaDescripcion)) {
-        return false;
-    }
+    bool copiedString = EntidadBase::copiarCadenaSeguro(
+        this->m_descripcion, sizeof(this->m_descripcion), nuevaDescripcion);
+
+    if (!copiedString) return false;
+
     return true;
 }
 
-bool Transaccion::setProductosIds(int productosIds[], int cantidades[], float preciosUnitarios[],
-                                  int cantidadTiposDeProductos)
+bool Transaccion::setProducto(TransaccionDTO nuevoProducto)
 {
-    this->cantidadTiposDeProductos = cantidadTiposDeProductos;
-    for (int i = 0; i < 100; ++i) {
-        this->productosIds[i] = productosIds[i];
-        this->cantidades[i] = cantidades[i];
-        this->preciosUnitarios[i] = preciosUnitarios[i];
-    }
-    return true;
-}
+    auto isDTOValid =
+        nuevoProducto.cantidad >= 0 && nuevoProducto.precio >= 0 && nuevoProducto.productoId >= 0;
 
-bool Transaccion::setCantidades(int cantidades[], float preciosUnitarios[],
-                                int cantidadTiposDeProductos)
-{
-    this->cantidadTiposDeProductos = cantidadTiposDeProductos;
-    for (int i = 0; i < 100; ++i) {
-        this->cantidades[i] = cantidades[i];
-        this->preciosUnitarios[i] = preciosUnitarios[i];
-    }
-    return true;
-}
+    if (!isDTOValid) return false;
 
-bool Transaccion::setPreciosUnitarios(float preciosUnitarios[], int cantidadTiposDeProductos)
-{
-    this->cantidadTiposDeProductos = cantidadTiposDeProductos;
-    for (int i = 0; i < 100; ++i) {
-        this->preciosUnitarios[i] = preciosUnitarios[i];
+    // if product already exists, sum its quantities
+    for (auto product : this->m_productos) {
+        if (product.productoId == nuevoProducto.productoId) {
+            product.cantidad += nuevoProducto.cantidad;
+            return true;
+        }
     }
+
+    // if product doesn't exists, append it
+    if (this->m_productosTotales >= 100) return false;
+
+    this->m_productos[this->m_productosTotales] = nuevoProducto;
+    this->m_productosTotales += 1;
+
     return true;
 }
--- src/domain/entities/transaccion/transaccion.entity.hpp
+++ src/domain/entities/transaccion/transaccion.entity.hpp
@@ -4,56 +4,53 @@
 
 enum TipoDeTransaccion { COMPRA, VENTA };
 
+struct TransaccionDTO {
+    int productoId;  // ID del producto
+    int cantidad;    // Cantidad de productos
+    int precio;      // Precio unitario
+};
+
 class Transaccion : public EntidadBase
 {
    private:
-    TipoDeTransaccion tipo;  // COMPRA o VENTA
-    int idRelacionado;       // ID del proveedor (compra) o cliente (venta)
-    float total;             // cantidad * precioUnitario
-    char descripcion[200];   // Notas adicionales (opcional)
+    TipoDeTransaccion m_tipo{};       // COMPRA o VENTA
+    int m_idRelacionado{0};           // ID del proveedor (compra) o cliente (venta)
+    float m_total{0};                 // cantidad * precioUnitario
+    char m_descripcion[200];          // Notas adicionales (opcional)
+    TransaccionDTO m_productos[100];  // Productos de la transaccion (hasta 100)
+    int m_productosTotales{0};        // cuantos productos existen en la transaccion
 
-    // Hasta 100 productos por transacción
-    int productosIds[100];
-    int cantidades[100];           // Cantidades de cada producto
-    float preciosUnitarios[100];   // Precios unitarios de cada producto
-    int cantidadTiposDeProductos;  // Cuantos elementos de los arrays anteriores
-                                   // se están usando
    public:
     Transaccion(int id, char* nombre, bool eliminado, time_point<system_clock> fechaCreacion,
                 time_point<system_clock> fechaUltimaModificacion, TipoDeTransaccion tipo,
-                int idRelacionado, float total, char* descripcion, int productosIds[],
-                int cantidades[], float preciosUnitarios[], int cantidadTiposDeProductos);
+                int idRelacionado, float total, char* descripcion, TransaccionDTO& productos);
 
-    auto getTipoTransaccion() const { return this->tipo; }
+    auto getTipoTransaccion() const { return this->m_tipo; }
 
     bool setTipoTransaccion(TipoDeTransaccion nuevoTipo);
 
-    auto getIdRelacionado() const { return this->idRelacionado; }
+    auto getIdRelacionado() const { return this->m_idRelacionado; }
 
     bool setIdRelacionado(int nuevoIdRelacionado);
 
-    auto getTotal() const { return this->total; }
+    auto getTotal() const { return this->m_total; }
 
     bool setTotal(float nuevoTotal);
 
-    auto getDescripcion() const { return this->descripcion; }
+    auto getDescripcion() const { return this->m_descripcion; }
 
     bool setDescripcion(char* nuevaDescripcion);
 
-    auto getProductosIds() const { return this->productosIds; }
-
-    bool setProductosIds(int productosIds[], int cantidades[], float preciosUnitarios[],
-                         int cantidadTiposDeProductos);
-
-    auto getCantidades() const { return this->cantidades; }
-
-    auto getPreciosUnitarios() const { return this->preciosUnitarios; }
-
-    auto getCantidadTiposDeProductos() const { return this->cantidadTiposDeProductos; }
-
-    bool setCantidadTiposDeProductos(int nuevaCantidadTiposDeProductos)
+    auto getProducto(int id) const
     {
-        this->cantidadTiposDeProductos = nuevaCantidadTiposDeProductos;
-        return true;
+        for (auto product : this->m_productos) {
+            if (product.productoId == id) {
+                return product;
+            }
+        }
+
+        throw std::invalid_argument("Producto no encontrado");
     }
+
+    bool setProducto(TransaccionDTO nuevoProducto);
 };
