dee23ec8eb96b8a6e2cb0931a8092ea8089d9d85
JonnerPaz <jonnerpazp@gmail.com>
2026-04-03 18:13:40 -0400

    feat(tienda): Add getters and setters

--- src/domain/entities/tienda/tienda.entity.hpp
+++ src/domain/entities/tienda/tienda.entity.hpp
@@ -1,13 +1,15 @@
 #pragma once
-#include "domain/entities/entidad.entity.hpp"
 #include <chrono>
 #include <cstring>
+
+#include "domain/entities/entidad.entity.hpp"
 
 using std::chrono::system_clock;
 using std::chrono::time_point;
 
-class Tienda : public EntidadBase {
-  private:
+class Tienda : public EntidadBase
+{
+   private:
     char rif[20];
     int totalProductosActivos;
     int totalProveedoresActivos;
@@ -16,11 +18,55 @@ class Tienda : public EntidadBase {
     float montoTotalVentas;
     float montoTotalCompras;
 
-  public:
+   public:
+    Tienda();
     Tienda(int id, const char* nombre, const char* rif, int totalProductosActivos,
            int totalProveedoresActivos, bool eliminado, int totalClientesActivos,
            time_point<system_clock> fechaCreacion, int montoTotalVentas, int montoTotalCompras,
            int totalTransaccionesActivas, time_point<system_clock> fechaUltimaModificacion);
+
+    const char* getRif() const { return rif; }
+
+    bool setRif(const char* rif);
+
+    int getTotalProductosActivos() const { return totalProductosActivos; }
+
+    void setTotalProductosActivos(int totalProductosActivos)
+    {
+        this->totalProductosActivos = totalProductosActivos;
+    }
+
+    int getTotalProveedoresActivos() const { return totalProveedoresActivos; }
+
+    void setTotalProveedoresActivos(int totalProveedoresActivos)
+    {
+        this->totalProveedoresActivos = totalProveedoresActivos;
+    }
+
+    int getTotalClientesActivos() const { return totalClientesActivos; }
+
+    void setTotalClientesActivos(int totalClientesActivos)
+    {
+        this->totalClientesActivos = totalClientesActivos;
+    }
+
+    int getTotalTransaccionesActivas() const { return totalTransaccionesActivas; }
+
+    void setTotalTransaccionesActivas(int totalTransaccionesActivas)
+    {
+        this->totalTransaccionesActivas = totalTransaccionesActivas;
+    }
+
+    float getMontoTotalVentas() const { return montoTotalVentas; }
+
+    void setMontoTotalVentas(float montoTotalVentas) { this->montoTotalVentas = montoTotalVentas; }
+
+    float getMontoTotalCompras() const { return montoTotalCompras; }
+
+    void setMontoTotalCompras(float montoTotalCompras)
+    {
+        this->montoTotalCompras = montoTotalCompras;
+    }
 
     ~Tienda() = default;
 };
